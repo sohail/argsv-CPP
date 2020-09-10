c4e44da59ce7405f5cb034e255d1d1ebdefe525f
sohail <sohailqayummalik@gmail.com>
2020-09-10 16:12:01 +0500

    Initial commit

---
+++ main.cc
@@ -0,0 +1,24 @@
+/*
+    main.cc
+    Written by, Sohail Qayum Malik soni@sharingeconomy.pk
+ */
+
+#include <iostream>
+#include "lib/csv/src/parser.hh"
+
+#define COMMAND "d,-d,dir,--dir,(displays a list of files and subdirectories in a directory)\nh,-h,help,--help,?,/?,(displays help screen)\nv,-v,version,--version,(displays version number)"
+
+int main(int argc, char* argv[])
+{  
+    cc_tokenizer::csv_parser<cc_tokenizer::String<char>, char> parser(cc_tokenizer::String<char>(COMMAND));
+    while (parser.go_to_next_line() != cc_tokenizer::string_character_traits<char>::eof())
+    {
+        std::cout<<parser.get_current_line().c_str()<<std::endl;
+        while (parser.go_to_next_token() != cc_tokenizer::string_character_traits<char>::eof())
+        {
+            std::cout<<parser.get_current_token().c_str()<<std::endl;
+        }
+    }
+
+    return 0;
+}
